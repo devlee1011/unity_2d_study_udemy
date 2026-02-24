4fd7396131814d1c537308df1887809a90f0f0ba
LeeJihyeon <devlee1011@gmail.com>
2026-02-24 23:46:34 +0900

    add a debug log

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("안녕 난 또치라고해 만나서 반가워");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
